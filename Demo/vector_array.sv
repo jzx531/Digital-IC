a03016da764a0d76d3c926d2f48ef137bb726b8b
zixian Jiang <115254510+jzx531@users.noreply.github.com>
2026-09-08 23:00:14 +0800

    Update vector_array.sv

--- Demo/vector_array.sv
+++ Demo/vector_array.sv
@@ -27,3 +27,45 @@ module tb3;
     end
 endmodule
 
+module tb4;
+    initial begin
+        bit [31:0] mem[ int unsigned];
+        int unsigned data,addr;
+        repeat(10) begin
+            std::randomize(addr,data) with {addr[31:8] == 0; addr[1:0] ==0 ;data inside {[1:10]};};
+            $display("addr=%d,data=%d",addr,data);
+            mem[addr] = data;
+            foreach(mem[i]) begin
+                $display("mem[%d]=%d",i,mem[i]);
+            end
+        end
+        if(mem.first(addr)) begin
+            do
+                $display("mem[%d]=%d",addr,mem[addr]);
+            while(mem.next(addr));
+        end
+        if(mem.exists('h10)) 
+            $display("mem[10] exists : %d",mem['h10]);
+        else
+            $display("mem[10] does not exist");
+    end
+endmodule
+
+module tb5;
+    initial begin
+        int dyn1[] = '{1,1,2,3,4,5,6};
+        int quel[$] = '{1,2,3,4,5,6};
+
+        int tq[$];
+        tq = q.min();
+        $display("Minimum value in quel: %d",tq[0]);
+        tq = d.max();
+        $display("Maximum value in dyn1: %d",tq[0]);
+        tq = dyn1.unique();
+        $display("Unique values in dyn1: %p",tq);
+        int d[] = '{4,5,8,9,3,6};
+        tq = d.sort();
+        $display("Sorted values in d: %p",tq);
+    end
+endmodule
+
