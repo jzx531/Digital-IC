c8f66a08f638265913b8d41e2bbd19509a17c15d
zixian Jiang <115254510+jzx531@users.noreply.github.com>
2026-09-08 22:35:09 +0800

    Update vector_array.sv

--- Demo/vector_array.sv
+++ Demo/vector_array.sv
@@ -7,4 +7,23 @@ module tb;
     end
 endmodule
 
+module tb2;
+    initial begin
+       int dyn[] = '{10,11,12,13,14};
+       $display("dyn contents: %p",dyn); 
+       dyn.delete();
+       $display("dyn contents after delete: %p",dyn);
+    end
+endmodule
+
+module tb3;
+    initial begin
+        int q1[$] = '{1,2,3};
+        int q2[$] = '{4};
+        q1.insert(1,4);
+        $display("q1 contents: %p",q1);
+        q1 = {q1[0] , q2 , q1[1:$]};
+        $display("q1 contents after insert and merge: %p",q1);
+    end
+endmodule
 
